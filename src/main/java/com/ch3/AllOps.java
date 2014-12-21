baec13bd83ef30542e9445cc79984da3cf386ce4
ilemm <2.igor.lemm>
2014-12-21 20:52:13 +0200

    WIP

--- src/main/java/com/ch3/AllOps.java
+++ src/main/java/com/ch3/AllOps.java
@@ -7,4 +7,15 @@ public class AllOps {
     public static void main(String[] args){
         System.out.println("Git!");
     }
+
+    void f(boolean b){}
+    void boolTest(boolean x, boolean y){
+        // Arithm oper
+        //!x=x*y;
+        //!x=x/y;
+        f(x==y);
+        f(x!=y);
+        f(!y);
+        x=x&&y;
+    }
 }
